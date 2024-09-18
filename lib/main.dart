f3b71e050a3cc7f973e1b5bf6895e519dbb531ae
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-09-19 02:42:16 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_my_app/login_page.dart';
+
+void mian() {
+  runApp(MaterialApp(
+debugShowCheckedModeBanner: false,
+home: LoginPage(),
+
+  ));
+}
